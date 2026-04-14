e49e2ec3d5ae0e2102639a074777cda209ebc315
AnyoneClown <96691442+AnyoneClown@users.noreply.github.com>
2026-04-14 21:03:46 +0300

    Add fields to Purchase Invoice sink (#8)

--- target_exact/sinks.py
+++ target_exact/sinks.py
@@ -388,6 +388,10 @@ class PurchaseInvoicesSink(ExactSink):
                             "Quantity": line.get("quantity"),
                             "Amount": line.get("totalPrice"),
                         }
+                        discount = line.get("discount")
+                        if discount is not None:
+                            invoice_line.update({"Discount": discount})
+
                         if line.get("taxCode"):
                             invoice_line.update({"VATCode": line.get("taxCode")})
 
